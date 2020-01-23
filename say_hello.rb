ca51ca67c4f45bd8e14c47c54fd2643135faa2f2
Yani Moore <yanimoore09@gmail.com>
2020-01-23 01:18:08 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -4,8 +4,7 @@
   #puts "hello #{name} we herd you were a grate #{languge}
   programmer
 
-#def greeting(name = "say_hello", language="Ruby")
+def greeting(name = "say_hello", language="Ruby")
   
- # puts "Hello, #{name}. We heard you are a great #{language} programmer."
-#end
-def say_hello("Gabriela")
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
