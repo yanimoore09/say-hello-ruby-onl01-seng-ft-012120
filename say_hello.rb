bfe68842cc1e3023ab9c0012ffe49c7930cddf42
Yani Moore <yanimoore09@gmail.com>
2020-01-23 01:18:44 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,10 @@
-# Build your say_hello method here
+
+
+#def say_hello(name = ruby, languge = programmer)
+  #puts "hello #{name} we herd you were a grate #{languge}
+  programmer
+
+def greeting(name = "say_hello", language="Ruby")
+  
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
